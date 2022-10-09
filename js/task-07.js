6a6628eccd3b0fd9f640bfcc0f45cdac24cea3d1
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2022-10-09 17:55:24 +0300

    task 7 fontSize change

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,11 @@ const controlFont = document.getElementById("font-size-control");
 
 const text = document.getElementById("text");
 
-controlFont.addEventListener('change', el => {
-    text.style.fontSize = `${el.target.value}px`
-});
+text.style.fontSize = `${controlFont.value}px`;
+
+controlFont.addEventListener('input', onInputChange);
+
+function onInputChange() {
+    const fontSize = Number(controlFont.value);
+    text.style.fontSize = `${fontSize}px`
+};
